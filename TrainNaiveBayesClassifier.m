705f758ada559c5b4401279ee56edccb48760b8a
Kai Chan <chankw.jh@gmail.com>
2017-03-21 16:43:57 +0000

    Update TrainNaiveBayesClassifier

--- TrainNaiveBayesClassifier.m
+++ TrainNaiveBayesClassifier.m
@@ -1,7 +1,7 @@
-%% Paper
+%% Paper %%
 %https://pdfs.semanticscholar.org/f376/711bbe5cc71089e0c74982cafed5f6bdae79.pdf?_ga=1.49846566.1631971690.1490113648
 
-%% Read Trainning Data
+%% Read Trainning Data %%
 close all; clear all; clc;
 path_root = 'C:/Users';
 addpath(genpath(path_root));
@@ -10,18 +10,18 @@ path_training_images1 = fullfile(path_root,'/THERMAL/train/positives');
 path_training_images2 = fullfile(path_root,'/THERMAL/train/negatives');
 path_testing_images = fullfile(path_root,'/THERMAL/test');
 
-
-files = dir(fullfile(path_training_images1,'*.png'));
-numberOfImages = length(files);
 index = 1;
 labels = [];
-%% Training positive %%
+%% Generating Positive DCT Descriptor %%
+files = dir(fullfile(path_training_images1,'*.png'));
+numberOfImages = length(files);
 for i_file = 1:numberOfImages
     % Read image
     image_file = fullfile(path_training_images1, files(i_file).name);
     im = imread(image_file);    
     im = imresize(im, [32 ,16]);
     descriptor = [];
+    %Sliding Window
     for r = 1:4:25
         for c = 1:4:9
             data = im(r:r+7, c:c+7);
@@ -36,7 +36,7 @@ for i_file = 1:numberOfImages
     index = index +1 ;
 end
 
-%% Training negative %%
+%% Generating Negative DCT Descriptor %%
 files = dir(fullfile(path_training_images2,'*.png'));
 numberOfImages = length(files);
 for i_file = 1:numberOfImages
@@ -45,6 +45,7 @@ for i_file = 1:numberOfImages
     im = imread(image_file);    
     im = imresize(im, [32 ,16]);
     descriptor = [];
+    %Sliding Window
     for r = 1:4:25
         for c = 1:4:9
             data = im(r:r+7, c:c+7);
@@ -58,5 +59,5 @@ for i_file = 1:numberOfImages
     labels = [labels, 0];
     index = index +1 ;
 end
-%% learning
+%% Train Classifier %%
 Mdl = fitcnb(table,labels');
