5a81a9131876682b15d0f25652d20c2ebb376f5c
Kai <chankw.jh@gmail.com>
2017-06-03 14:05:05 +0100

    update

--- Ellipse.m
+++
@@ -1,6 +0,0 @@
-  function [x y] = Ellipse(cx, cy, axx, axy, ang)
-  %generate the outline points of a ellipse
-    theta = 0 : 0.0001 : 2*pi;
-    x = axy/2 * cos(theta) * cos(ang) - axx/2 * sin(theta) * sin(ang) + cx;
-    y = axx/2 * sin(theta) * cos(ang) + axy/2 * cos(theta) * sin(ang) + cy;
-  end
